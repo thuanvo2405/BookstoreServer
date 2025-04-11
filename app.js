33014c9d8784d5fb55ed9ac683c839a68d6be925
thuanvo2405 <vothuan2405003@gmail.com>
2025-04-11 17:54:14 +0700

    get Anh

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
+const cloudinary = require("./config/cloudinary");
+
 const swaggerSetup = require("./config/swagger");
 dotenv.config();
 
 app.use(express.json());
 app.use(cors());
 swaggerSetup(app);
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const hangHoaRoutes = require("./routes/hangHoa.routes");
 const loaiHangHoaRoutes = require("./routes/loaiHangHoa.routes");
@@ -26,6 +31,30 @@ app.use("/api/hoadon", hoaDonRoutes);
 app.use("/api/chitietphieuxuat", chiTietPhieuXuatRoutes);
 app.use("/api/nhanvien", nhanVienRoutes);
 app.use("/api/khachhang", khachHangRoutes);
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/"); // ảnh sẽ lưu vào thư mục uploads
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + file.originalname;
+    cb(null, uniqueSuffix); // ví dụ: 1712821234-meow.png
+  },
+});
+const upload = multer({ storage: storage });
+
+// API nhận ảnh
+app.post("/upload", upload.single("image"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ message: "Không có file nào được upload" });
+  }
+
+  res.json({
+    message: "Upload thành công!",
+    fileName: req.file.filename,
+    filePath: `/uploads/${req.file.filename}`,
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
